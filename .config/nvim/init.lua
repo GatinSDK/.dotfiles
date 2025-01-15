a4bb637312c3557e235e3b0a1ba00bc427cf2389
Gatin <gatinslaughter@gmail.com>
2025-01-15 10:30:53 -0400

    update nvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -54,6 +54,8 @@ vim.diagnostic.config({
 
 vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
+vim.keymap.set("n", "<leader>d", '"_d', { desc = "Delete to void" })
+
 -- Disable arrow keys in normal mode
 vim.keymap.set("n", "<left>", '<cmd>echo "Use h to move!!"<CR>')
 vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
